f14daad08f646bd4e9283ccf1521efeaf38637ef
Pavan <swysoft@gmail.com>
2019-02-24 16:18:21 +0530

    Add customer table

--- resources/scripts.js
+++ resources/scripts.js
@@ -1,52 +1,5 @@
-var products = [
-    {
-        sectionTitle: "UNIVERSAL TESTING MACHINES",
-        images: [
-            {src:"Mechanical cum Computerised UTM with Digital Extensometer 100 tonnes capacity.jpg", imageTitle:"Mechanical cum Computerised UTM with Digital Extensometer 2000 kn capacity"},
-            {src:"Analogue type UTM 100 kn to 1000 kn.jpg", imageTitle:"Analogue type UTM 100 kn to 2000 kn"},
-            {src:"Microprocessor type UTM 40 tonnes capacity.jpg", imageTitle:"Microprocessor type UTM 40 tonnes capacity"},
-            {src:"Analogue type UTM 60 tonnes capacity.jpg", imageTitle:"Analogue type UTM 60 tonnes capacity"},
-            {src:"Computerised Servo control Hydraulic UTM with Electronic Extensometer.jpg", imageTitle:"Computerised Servo control Hydraulic UTM with Electronic Extensometer"},
-            {src:"Microprocessor cum Computerised laminated & coil Spring Testing Machine.jpg", imageTitle:"Microprocessor cum Computerised laminated & coil Spring Testing Machine"},
-            {src:"Digital Manhole Cover Testing Machine.png", imageTitle:"Digital Manhole Cover Testing Machine"}
-
-        ]
-    },
-
-    {
-        sectionTitle: "Accessories for UNIVERSAL TESTING MACHINES",
-        images: [
-            {src:"Electronic Extensometer.jpg", imageTitle: "Electronic Extensometer"},
-            {src:"180 degree direct bend test attachment for TMT Bars.jpg", imageTitle:"180 degree direct bend test attachment for TMT Bars"},
-            {src:"Bend & Rebend Pane.jpg", imageTitle:"Bend & Rebend Pane"},
-            {src:"Bend & Rebend Test Panes & Mandrels.jpg", imageTitle:"Bend & Rebend Test Panes & Mandrels"},
-            {src:"V Table 600 mm long.jpg", imageTitle:"V Table 600 mm long"},
-            {src:"Water Heating arrangment for TMT rebend test.jpg", imageTitle:"Water Heating arrangment for TMT rebend test"},
-            {src:"Roller Stand for Manhole Cover Testing Machine.jpg", imageTitle:"Roller Stand for Manhole Cover Testing Machine"}
-        ]
-    },
-
-    {
-        sectionTitle:"DYNAMIC BALANCING MACHINES",
-        images: [
-            {src:"Digital Dynamic Balancing Machine 3 tonnes capacity.jpg", imageTitle:"Digital Dynamic Balancing Machine 3 tonnes capacity"},
-            {src:"Digital Dynamic Balancing Machine 7 tonnes capacity.jpg", imageTitle:"Digital Dynamic Balancing Machine 7 tonnes capacity"},
-            {src:"Computerised Dynamic Balancing Machine 7 tonnes capacity.jpg", imageTitle:"Computerised Dynamic Balancing Machine 7 tonnes capacity"},
-            {src:"End cum Belt Drive Computerised Balancing Machine with VFD(Variable Frequency Drive) 10 tonnes capacity.jpg", imageTitle:"End cum Belt Drive Computerised Balancing Machine with VFD(Variable Frequency Drive) 10 tonnes capacity"}
-        ]
-    },
-
-    {
-        sectionTitle: "HARDNESS TESTING MACHINES",
-        images: [
-            {src:"Brinell Hardness Tester 3000 kgf with Comparetor.jpg", imageTitle:"Brinell Hardness Tester 3000 kgf with Comparetor"},
-            {src:"Computerised Brinell Hardness Tester 3000 kgf.jpg", imageTitle:"Computerised Brinell Hardness Tester 3000 kgf"},
-            {src:"Rockwell Hardness Tester.jpg", imageTitle:"Rockwell Hardness Tester"}
-        ]
-    }
-];
-
 var basePath = "./resources/img/";
+var displayCustomers = "Y"
 
 $(function(){
     console.log(products);
@@ -94,7 +47,16 @@ $(function(){
             imgPopup(title, imgContent);
     });
 
-})
+});
+
+$(function(){
+    refCustList = $("div#refCustomerList div.customers-pdt");
+    targetDiv = $("div#customerData");
+    $.each(customerData, function(index, customersDataSinglePdt){
+        loadCustomers(customersDataSinglePdt, refCustList, targetDiv);
+    });
+    
+});
 
 function imgPopup(title, content){
     $("div#img-popup").find("h4.modal-title").html(title);
@@ -103,4 +65,35 @@ function imgPopup(title, content){
     $("div#img-popup").find("div.modal-body").append(content.clone());
 
     $("div#img-popup").modal("show");
+}
+
+function loadCustomers(customerData, templateDiv, targetDiv){
+        customerList = $(templateDiv).clone();
+        $(customerList).find("h3").html(customerData.tableTitle);
+        $.each(customerData.tableHeader, function(index, header){
+            $(customerList).find("table > thead > tr").append(
+                $("<th></th>").html(header)
+            );
+        });
+
+        $.each(customerData.tableData, function(index, rowData){
+           $(customerList).find("table > tbody").append(
+               $("<tr></tr>")
+           ); 
+
+           $.each(rowData, function(index, field){
+            $(customerList).find("table > tbody > tr:last").append(
+                $("<td></td>").html(field)
+            ); 
+           });
+        });
+
+        $(targetDiv).append(customerList);
+
+
+}
+
+function dispCustomers(){
+    $("div#dispProductList").addClass("hidden");
+    $("div#dispCustomerList").removeClass("hidden");
 }
